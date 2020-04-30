6c824cb6b192f389cec02898b4da3d5795fe6e99
SollWar <63407355+SollWar@users.noreply.github.com>
2020-04-30 16:58:06 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLDrevoString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_text = System.IO.File.ReadAllText(@"big.txt");
+            //string[] input_check = System.IO.File.ReadAllLines(@"check.txt");
+            System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+            long elapsedMs;
+
+            /// 
+            // the : 123
+            // red : 456
+            // ...
+            // count: 1456
+            // time:  4134
+
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("time:  " + elapsedMs);
+        }
+    }
+}
